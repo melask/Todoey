529eb71fd24f3a59a86bf2f7ea8e8241b8253b66
Konstantinos Melabianakis <16836475+melask@users.noreply.github.com>
2019-03-02 11:07:13 +0200

    Codable protocol to save data

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -12,30 +12,16 @@ class TodoListViewController: UITableViewController {
 
   var itemArray = [Item]()
   
-  let defaults = UserDefaults.standard
+  let dataFilePath = FileManager.default.urls(for: .documentDirectory,
+                                              in: .userDomainMask).first?.appendingPathComponent("Items.plist")
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     
     print(dataFilePath)
     
-    let newItem = Item()
-    newItem.title = "Find Mike"
-    itemArray.append(newItem)
-    
-    let newItem2 = Item()
-    newItem2.title = "Buy Eggs"
-    itemArray.append(newItem2)
-    
-    let newItem3 = Item()
-    newItem3.title = "Destroy Demogorgon"
-    itemArray.append(newItem3)
-    
-    if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
-      itemArray = items
-    }
+    loadItems()
   }
 
   //MARK - Tableview Datasource Methods
@@ -61,9 +47,9 @@ class TodoListViewController: UITableViewController {
   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     
     itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+
+    saveItems()
     
-    
-    tableView.reloadData()
     tableView.deselectRow(at: indexPath, animated: true)
   }
   
@@ -82,9 +68,7 @@ class TodoListViewController: UITableViewController {
       
       self.itemArray.append(newItem)
       
-      self.defaults.set(self.itemArray, forKey: "TodoListArray")
-      
-      self.tableView.reloadData()
+      self.saveItems()
     }
     
     alert.addTextField { (alertTextField) in
@@ -95,6 +79,34 @@ class TodoListViewController: UITableViewController {
     alert.addAction(action)
     
     present(alert, animated: true, completion: nil)
+  }
+  
+  // MARK - Model manipulation mathods
+  
+  func saveItems() {
+    let encoder = PropertyListEncoder()
+    
+    do {
+      let data = try encoder.encode(self.itemArray)
+      try data.write(to: self.dataFilePath!)
+    } catch {
+      print("Error encoding item array, \(error)")
+    }
+    
+    self.tableView.reloadData()
+    
+  }
+  
+  func loadItems() {
+    if let data = try? Data(contentsOf: dataFilePath!) {
+      let decoder = PropertyListDecoder()
+      do {
+        itemArray = try decoder.decode([Item].self, from: data)
+      } catch {
+        print ("Error decoding item array, \(error)")
+      }
+      
+    }
   }
   
 
