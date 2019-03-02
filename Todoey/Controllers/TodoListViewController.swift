4a9979f293ba1ca9856cf89af896d7419535e71c
Konstantinos Melabianakis <16836475+melask@users.noreply.github.com>
2019-03-02 14:06:00 +0200

    Searchbar

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -7,24 +7,24 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoListViewController: UITableViewController {
 
   var itemArray = [Item]()
   
-  let dataFilePath = FileManager.default.urls(for: .documentDirectory,
-                                              in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+  
+  let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    
-    print(dataFilePath)
+    print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
     
     loadItems()
   }
 
-  //MARK - Tableview Datasource Methods
+  //MARK: - Tableview Datasource Methods
   
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return itemArray.count
@@ -42,18 +42,21 @@ class TodoListViewController: UITableViewController {
     return cell
   }
   
-  //MARK - TableView Delegate Methods
+  //MARK: - TableView Delegate Methods
   
   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    
-    itemArray[indexPath.row].done = !itemArray[indexPath.row].done
 
+    context.delete(itemArray[indexPath.row])
+    itemArray.remove(at: indexPath.row)
+
+    // itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+  
     saveItems()
     
     tableView.deselectRow(at: indexPath, animated: true)
   }
   
-  //MARK - Add New Items
+  //MARK: - Add New Items
   
   @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
     var textField = UITextField()
@@ -63,9 +66,9 @@ class TodoListViewController: UITableViewController {
     let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
       //what will happen once the user clicks the Add Item on our UIAlert
       
-      let newItem = Item()
+      let newItem = Item(context: self.context)
       newItem.title = textField.text!
-      
+      newItem.done = false
       self.itemArray.append(newItem)
       
       self.saveItems()
@@ -81,34 +84,52 @@ class TodoListViewController: UITableViewController {
     present(alert, animated: true, completion: nil)
   }
   
-  // MARK - Model manipulation mathods
+  //MARK: - Model manipulation mathods
   
   func saveItems() {
-    let encoder = PropertyListEncoder()
-    
+   
     do {
-      let data = try encoder.encode(self.itemArray)
-      try data.write(to: self.dataFilePath!)
+      try context.save()
     } catch {
-      print("Error encoding item array, \(error)")
+      print("Error saving context \(error)")
     }
     
     self.tableView.reloadData()
     
   }
   
-  func loadItems() {
-    if let data = try? Data(contentsOf: dataFilePath!) {
-      let decoder = PropertyListDecoder()
-      do {
-        itemArray = try decoder.decode([Item].self, from: data)
-      } catch {
-        print ("Error decoding item array, \(error)")
+  func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
+    do {
+      itemArray = try context.fetch(request)
+    } catch {
+      print("Error fetching data from context \(error)")
+    }
+    
+    tableView.reloadData()
+  }
+}
+
+//MARK: - Search bar methods
+extension TodoListViewController: UISearchBarDelegate {
+  
+  func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+    let request : NSFetchRequest<Item> = Item.fetchRequest()
+    
+    request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+    request.sortDescriptors = [ NSSortDescriptor(key: "title", ascending: true)]
+    
+    loadItems(with: request)
+  }
+  
+  func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+    if searchBar.text?.count == 0 {
+      loadItems()
+      
+      DispatchQueue.main.async {
+        searchBar.resignFirstResponder()
       }
       
     }
   }
   
-
 }
-
